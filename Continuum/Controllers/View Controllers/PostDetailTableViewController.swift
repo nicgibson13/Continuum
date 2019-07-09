2a90676289784c9a50a4810cf1aa0f0d4c06dbe8
Nic Gibson <nic@shpangy.com>
2019-07-09 17:11:16 -0600

    Implemented PostDetailController, added notification for user input of comment

--- Continuum/Controllers/View Controllers/PostDetailTableViewController.swift
+++ Continuum/Controllers/View Controllers/PostDetailTableViewController.swift
@@ -8,83 +8,78 @@
 
 import UIKit
 
-class PostDetailTableViewController: UITableViewController {
+class PostDetailTableViewController: UITableViewController, UITextFieldDelegate {
 
+    var postLandingPad: Post? {
+        didSet {
+            DispatchQueue.main.async {
+                self.updateViews()
+            }
+        }
+    }
+    
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+       
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    @IBAction func commentButtonTapped(_ sender: Any) {
+        let alertController = UIAlertController(title: "Leave a comment 🤪", message: nil, preferredStyle: .alert)
+        alertController.addTextField { (textField) -> Void in
+            textField.delegate = self
+            textField.placeholder = "Insert comment..."
+            textField.autocorrectionType = .yes
+            textField.autocapitalizationType = .sentences
+        }
+        
+        let addComment = UIAlertAction(title: "OK", style: .default) { (_) in
+            guard let comment = alertController.textFields?.first?.text,
+                let post = self.postLandingPad else {return}
+            if comment != "" {
+                PostController.sharedInstance.addComment(text: comment, post: post, completion: { (comment) in
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
+                    }
+                })
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title:"Cancel", style: .destructive)
+        
+        alertController.addAction(addComment)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true)
+    }
+    
+    @IBAction func shareButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func followPostButtonTapped(_ sender: Any) {
+    
+    }
+    
+    func updateViews() {
+        photoImageView.image = postLandingPad?.photo
+        self.tableView.reloadData()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        guard let comments = postLandingPad?.comments else {return 0}
+        return comments.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "commentCell", for: indexPath)
+        let comments = postLandingPad?.comments[indexPath.row]
+        cell.textLabel?.text = comments?.text
+        cell.detailTextLabel?.text = comments?.timestamp.formatDate()
+        
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ Continuum/Storyboards + Views/PostTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  PostTableViewCell.swift
+//  Continuum
+//
+//  Created by Nic Gibson on 7/9/19.
+//  Copyright © 2019 Nic Gibson. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var postCaptionLabel: UILabel!
+    @IBOutlet weak var postCommentLabel: UILabel!
+    
+    var post: Post? {
+        didSet {
+            DispatchQueue.main.async {
+                self.updateViews()
+            }
+        }
+    }
+    
+    func updateViews() {
+        guard let post = post else {return}
+        postImageView.image = post.photo
+        postCaptionLabel.text = post.caption
+        postCommentLabel.text = "\(post.comments.count)"
+    }
+
+}
